860565acc8fd6965cab5e9be5e0abda0933bd110
caidicaidi123 <caidicaidi123@gmail.com>
2018-10-23 23:56:27 +0800

    mock data for visualization 2.1

---
+++ src/mocks/ScoreRecommendationMock.js
@@ -0,0 +1,30 @@
+const dummyLabels = [
+  '20',
+  '19',
+  '18',
+  '17',
+  '16',
+  '15',
+  '14',
+  '13',
+  '12',
+  '11',
+];
+
+const dummyData = [
+  50,
+  45,
+  40,
+  35,
+  30,
+  25,
+  20,
+  15,
+  10,
+  5
+];
+
+module.exports = {
+  dummyLabels,
+  dummyData
+};

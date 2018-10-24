1cc103b9473a7d1b0d486ccf843734c2c3490934
Cai Di <caidicaidi123@gmail.com>
2018-10-24 12:57:34 +0800

    Feature visualization 4.1 (#6)
    * first commit
    
    * server responses
    
    * implementation for visualization 4.1

---
+++ src/mocks/AuthorResponse.js
@@ -0,0 +1,20 @@
+const author_response = {
+  "infoData": {
+    "topCountries": {
+      "labels": ["United States", "China", "Germany", "India", "United Kingdom", "Japan", "Ireland", "Canada", "Netherlands", "New Zealand"],
+      "data": [200, 106, 97, 45, 20, 17, 14, 13, 13, 11]
+    },
+    "topAffiliations": {
+      "labels": ["Old Dominion University", "University of North Texas", "Wuhan University", "University of Illinois at Urbana-Champaign", "Trinity College Dublin", "University of Konstanz", "Indian Institute of Technology Patna", "School of Information Management, Wuhan University", "Nanjing Agricultural University", "National Science Library, Chinese Academy of Sciences"],
+      "data": [31, 29, 22, 16, 14, 12, 11, 11, 10, 10]
+    },
+    "topAuthors": {
+      "labels": ["Micxxxx Nexxxx", "Micxxxx Wexxxx", "J. xxxxxxx Doxxxx", "Anixxxx Mukxxxxxx", "Daxxx Baixxxxxxx", "Corxxxx Brexxxxxxx", "Bexx Gixx", "Shexxxx Dexx", "Jxx Haxx", "Wxx Lx"],
+      "data": [7, 7, 6, 5, 5, 4, 4, 4, 4, 4]
+    }
+  }, "infoType": "author"
+};
+
+module.exports = {
+  author_response
+};
---
+++ src/mocks/TopAcceptedAffiliationMock.js
@@ -0,0 +1,10 @@
+
+
+const dummyTopAcceptedAffiliationData = {
+  labels: ["Old Dominion University", "University of North Texas", "Wuhan University", "University of Illinois at Urbana-Champaign", "Trinity College Dublin", "University of Konstanz", "Indian Institute of Technology Patna", "School of Information Management, Wuhan University", "Nanjing Agricultural University", "National Science Library, Chinese Academy of Sciences"],
+    data: [31, 29, 22, 16, 14, 12, 11, 11, 10, 10]
+}
+
+module.exports = {
+  dummyTopAcceptedAffiliationData
+};

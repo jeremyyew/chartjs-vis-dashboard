d09bbc9d406b9c09a4ca93bb433b135b47c57e34
Li Hao Tan <LiHaoTan@users.noreply.github.com>
2018-11-09 22:27:26 +0800

    Add models to admin interface

--- backend/dataanalysis/admin.py
+++ backend/dataanalysis/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserFile, Author, Submission, Review
+
+admin.site.register(UserFile)
+admin.site.register(Author)
+admin.site.register(Submission)
+admin.site.register(Review)

ca6862651c4e13b55ceff98d537af793a706dfd2
Li Hao Tan <LiHaoTan@users.noreply.github.com>
2018-11-09 23:51:19 +0800

    Modifying in progress

--- backend/dataanalysis/urls.py
+++ backend/dataanalysis/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
 
     path('parse/', views.parseCSV, name='parse'),
 
-    path('getAuthorInfo/', getInsight.getAuthorInfo, name='getAuthorInfo'),
+    path('getAuthorInfo/', views.getAuthorInfo, name='getAuthorInfo'),
     path('getReviewInfo/', getInsight.getReviewInfo, name='getReviewInfo'),
     path('getSubmissionInfo/', getInsight.getSubmissionInfo, name='getSubmissionInfo'),
 
--- backend/dataanalysis/views.py
+++ backend/dataanalysis/views.py
@@ -1,9 +1,12 @@
+import json
+from collections import Counter
+
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 from rest_framework import status, serializers
 from rest_framework.decorators import api_view, permission_classes
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 
 from rest_framework.exceptions import APIException
 from rest_framework.permissions import AllowAny
@@ -96,6 +99,55 @@ def parseCSV(request):
         previewData.append(rowData)
 
     return JsonResponse({'data' : rowCSV, 'previewData': previewData})
+
+
+@api_view(['POST'])
+@permission_classes((AllowAny, ))
+def getAuthorInfo(request):
+    """
+    author.csv: header row, author names with affiliations, countries, emails
+    data format:
+    submission ID | f name | s name | email | country | affiliation | page | person ID | corresponding?
+    """
+
+    # TODO: use rest framework request.data
+    if request.body:
+        data = json.loads(request.body)
+        authorData = data['data'][1:]
+        authorData = [ele for ele in authorData if ele]
+        firstNameIndex = data['firstNameIndex']
+        lastNameIndex = data['lastNameIndex']
+        countryIndex = data['countryIndex']
+        affiliationIndex = data['affiliationIndex']
+
+        authorList = []
+        for authorInfo in authorData:
+            authorList.append({'name': authorInfo[firstNameIndex] + " " + authorInfo[lastNameIndex],
+                               'country': authorInfo[countryIndex],
+                               'affiliation': authorInfo[affiliationIndex]})
+
+        parsedResult = {};
+
+        authors = [ele['name'] for ele in authorList if
+                   ele]  # adding in the if ele in case of empty strings; same applies below
+        topAuthors = Counter(authors).most_common(10)
+        parsedResult['topAuthors'] = {'labels': [ele[0] for ele in topAuthors], 'data': [ele[1] for ele in topAuthors]}
+
+        countries = [ele['country'] for ele in authorList if ele]
+        topCountries = Counter(countries).most_common(10)
+        parsedResult['topCountries'] = {'labels': [ele[0] for ele in topCountries],
+                                        'data': [ele[1] for ele in topCountries]}
+
+        affiliations = [ele['affiliation'] for ele in authorList if ele]
+        topAffiliations = Counter(affiliations).most_common(10)
+        parsedResult['topAffiliations'] = {'labels': [ele[0] for ele in topAffiliations],
+                                           'data': [ele[1] for ele in topAffiliations]}
+
+        return JsonResponse({'infoType': 'author', 'infoData': parsedResult})
+
+    else:
+        print('Unable to find author data!')
+        return HttpResponseNotFound('Page not found for CSV')
 
 
 class UserSerializer(serializers.ModelSerializer):
